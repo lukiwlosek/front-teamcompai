891cfb6a59861cc28b1606a6d93bfe14054c9702
lukiwlosek <pan.lukasz.wlosek@gmail.com>
2023-04-16 10:43:25 +0100

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'TeamcompAI',
+  description: 'Made by Lukasz W',
+}
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="h-screen bg-sky-950">{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+import { json } from "stream/consumers";
+
+export default async function HomePage() {
+  const data = await getData()
+  console.log(data)
+    return (
+      <div className="container">
+      <h1>{JSON.stringify(data)}</h1>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-rows-5 gap-4">
+            <h1 className= "text-3xl font-bold">Blue Team</h1>
+            <div className="box-border h-16 w-64 p-4 border-4">
+              <div>
+                <input className="enabled:hover:border-gray-400 disabled:opacity-75 ..." />
+              </div>
+            </div>
+            <div className="box-border h-16 w-64 p-4 border-4">
+              <input className="enabled:hover:border-gray-400 disabled:opacity-75 ..." />
+            </div>
+            <div className="box-border h-16 w-64 p-4 border-4">
+               <input className="enabled:hover:border-gray-400 disabled:opacity-75 ..." />
+            </div>
+            <div className="box-border h-16 w-64 p-4 border-4">
+ 
+            </div>
+            <div className="box-border h-16 w-64 p-4 border-4">
+
+            </div>
+          </div>
+          <div><h1 className="text-3xl font-bold">Overview</h1></div>
+          <div><h1 className="text-3xl font-bold">Red Team</h1></div>
+        </div>
+      </div>
+    
+  );
+}
+
+// get champion list
+async function getData() {
+  const res = await fetch('http://ddragon.leagueoflegends.com/cdn/13.7.1/data/en_US/champion.json');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  return res.json();
+}
+
